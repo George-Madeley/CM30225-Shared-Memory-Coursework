5d243232ecad2aa02c871ba4bdeddc15e1dab156
George Madeley <prof.chewbacca@gmail.com>
2022-11-01 18:32:57 +0000

    Updated Results Name

--- test.py
+++ test.py
@@ -2,13 +2,17 @@ import subprocess
 import sys
 import csv
 import time
+from datetime import datetime
 
 def main():
   MAX_THREAD_NUM = int(sys.argv[1])
   NUM_OF_TESTS = int(sys.argv[2])
   ARRAY_SIZE = 100
 
-  with open('results.csv', 'w', newline='') as results_file:
+  current_datetime = datetime.now()
+  dt_string = current_datetime.strftime("%d-%m-%Y %H%M%S")
+
+  with open(f'results/results {dt_string}.csv', 'w', newline='') as results_file:
     test_filed_names = ['time ' + str(i + 1) for i in range(NUM_OF_TESTS)]
     field_names = ['num of threads'] + test_filed_names + ['average time']
     writer = csv.DictWriter(results_file, fieldnames=field_names)
@@ -24,7 +28,7 @@ def main():
         end_time = time.perf_counter()
         time_diff = end_time - start_time
         times.append(time_diff)
-      times_dict = {'time ' + str(i + 1): times[i] for i in range(NUM_OF_TESTS)}
+      times_dict = {'time ' + str(i + 1): round(times[i], 5) for i in range(NUM_OF_TESTS)}
       row_dict = {'num of threads': thread_num} | times_dict | {'average time': sum(times)/len(times)}
       writer.writerow(row_dict)
 
--- version_history.md
+++ version_history.md
@@ -1,5 +1,22 @@
 # Version History
 
+## 0.12
+
+Updated the results logging file. Will now no longer overwrite the previous results file.
+
+## 0.11
+
+- Included timing functions in .c
+- Fixed "pthread" include error.
+
+## 0.10
+
+Implimented a Python batch test script
+
+## 0.02
+
+First successfully execution of the program.
+
 ## 0.01
 
 Init Repo
