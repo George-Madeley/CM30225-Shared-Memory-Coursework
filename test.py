fb992349249f542e4a9cdd1d61eb957ec50bd01e
George Madeley <prof.chewbacca@gmail.com>
2022-11-04 12:17:10 +0000

    Improved Python Batch Tester

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import sys
 import csv
 import time
 from datetime import datetime
+from statistics import stdev
 
 def main():
   MAX_THREAD_NUM = int(sys.argv[1])
@@ -13,8 +14,7 @@ def main():
   dt_string = current_datetime.strftime("%d-%m-%Y %H%M%S")
 
   with open(f'results/results {dt_string}.csv', 'w', newline='') as results_file:
-    test_filed_names = ['time ' + str(i + 1) for i in range(NUM_OF_TESTS)]
-    field_names = ['num of threads'] + test_filed_names + ['average time']
+    field_names = ['num of threads', 'lowest time', 'highest time', 'average time', 'stdev time']
     writer = csv.DictWriter(results_file, fieldnames=field_names)
     writer.writeheader()
 
@@ -22,14 +22,19 @@ def main():
       times = []
       print(f"===== thread number {thread_num} =====")
       for test_num in range(1, NUM_OF_TESTS + 1):
-        print(f"test {test_num}")
-        start_time = time.perf_counter()
+        start_time = time.time_ns()
         subprocess.run(['./main.exe', str(thread_num), str(ARRAY_SIZE), '0'])
-        end_time = time.perf_counter()
-        time_diff = end_time - start_time
+        end_time = time.time_ns()
+        time_diff = (end_time - start_time) / (10 ** 9)
         times.append(time_diff)
-      times_dict = {'time ' + str(i + 1): round(times[i], 5) for i in range(NUM_OF_TESTS)}
-      row_dict = {'num of threads': thread_num} | times_dict | {'average time': sum(times)/len(times)}
+        print(f"test {test_num}\t\t\t time: {time_diff}")
+      row_dict = {
+        'num of threads': thread_num,
+        'lowest time': min(times),
+        'highest time': max(times),
+        'average time': sum(times)/len(times),
+        'stdev time': stdev(times)
+      }
       writer.writerow(row_dict)
 
 if __name__ == "__main__":
